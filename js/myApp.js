c260831ef6ad945868d1d7cc2156c63a3a4462b6
dtrinh888 <dtrinh888@gmail.com>
2015-12-02 19:13:46 -0800

    all calculations work

--- js/myApp.js
+++ js/myApp.js
@@ -3,19 +3,43 @@ angular.module('myApp', ["ngMessages"])
 		$scope.mealPrice = '';
 		$scope.taxRate = '';
 		$scope.tipPercentage = '';
+		$scope.tipTotal = 0;
+		$scope.mealCount = 0;
+		$scope.subtotal = 0;
+		$scope.tip = 0;
+		$scope.total = 0;
 		$scope.submitMeal = function(){
 			if ($scope.mealDetails.$valid) {
 				$scope.subtotal = $scope.mealPrice * (1+ ($scope.taxRate/100));
 				$scope.tip = $scope.subtotal * ($scope.tipPercentage/100);
 				$scope.total = $scope.subtotal + $scope.tip;
+				$scope.tipTotal += $scope.tip;
+				$scope.mealCount++;
+				$scope.mealPrice = '';
+				$scope.taxRate = '';
+				$scope.tipPercentage = '';
 			}
 		};
-		$scope.cancelMeal = function() {
+		$scope.cancelMeal = function(){
 			$scope.mealPrice = '';
 			$scope.taxRate = '';
 			$scope.tipPercentage = '';
 			$scope.subtotal = '';
 			$scope.tip = '';
 			$scope.total = '';
+		};
+		$scope.avgTipTotal = function(){
+			if ($scope.mealCount !== 0){
+				return $scope.tipTotal / $scope.mealCount;
+			} else {
+				return 0;
+			}
+		};
+		$scope.reset = function(){
+			$scope.mealDetails.$setPristine();
+			$scope.mealDetails.$setUntouched();
+			$scope.cancelMeal();
+			$scope.totalTip = 0;
+			$scope.mealCount = 0;
 		};
 	}]);
