f94de353da9c4804df2e1d22ed2142d980627d66
dtrinh888 <dtrinh888@gmail.com>
2016-06-28 18:56:32 -0700

    changed route

--- js/myApp.js
+++ js/myApp.js
@@ -1,7 +1,7 @@
 angular.module('myApp', ['ngMessages', 'ngRoute', 'ngAnimate'])
 	.config(['$routeProvider', function($routeProvider){
 		$routeProvider.when('/', {
-			templateUrl: 'home.html',
+			templateUrl: 'index.html',
 			controller: 'HomeCtrl'
 		})
 		.when('/new-meal', {
